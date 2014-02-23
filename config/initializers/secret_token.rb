63cdd694d69afac2013a0fd0de551ac3ab396a50
Alexander Potashev <aspotashev@gmail.com>
2014-02-23 20:06:21 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koverage::Application.config.secret_key_base = '51bd61a6bdc66e7ab6e6955937ff9921c2ee75c79ab4a4a6eb871a8c91dcfcbda2359272a1411a9fb81a9b3fd26a76f356877deee3015d73902bf870949c679f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Koverage::Application.config.session_store :cookie_store, key: '_koverage_session'
